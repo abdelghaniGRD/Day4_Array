062c9382e56ea127ec25f39cae5bb10ffcb0109f
abdel-ghani <gouroudabdelghani@gmail.com>
2026-09-22 12:59:45 +0100

    providing second solution for exercice 27

--- 27.js
+++ 27.js
@@ -1,6 +1,25 @@
 let arr = [1, 2, 3];
-let newArr = [];
-for (let i = arr.length - 1; i >= 0; i--) {
-  newArr.push(arr[i]);
+
+/*SOLUTION ONE */
+
+// let newArr = [];
+// for (let i = arr.length - 1; i >= 0; i--) {
+//   newArr.push(arr[i]);
+// }
+// console.log(newArr);
+
+/*SOLUTION TWO */
+
+let i = 0,
+  j = arr.length - 1;
+
+while (i !== j) {
+  let temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+
+  i++;
+  j--;
 }
-console.log(newArr);
+
+console.log(arr);
