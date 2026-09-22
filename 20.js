de850b884d65712961cae75f3415ddce2011fce6
abdel-ghani <gouroudabdelghani@gmail.com>
2026-09-22 12:47:34 +0100

    correct a logical error, we should devide by the lenght of the array minus 1 wich is the max

--- 20.js
+++ 20.js
@@ -3,22 +3,17 @@ let arr = [-3, 5, -1, 8, 0];
 let max;
 let sum = 0;
 
-for (let i in arr) {
-  if (i == 0) max = arr[i];
+function AverageExMax(T) {
+  max = T[0];
+  for (let i in T) {
+    if (T[i] > max) {
+      max = T[i];
+    }
 
-  if (arr[i] > max) {
-    max = arr[i];
+    sum += T[i];
   }
-
-  sum += arr[i];
+  sum -= max;
+  return sum / (T.length - 1);
 }
 
-console.log("sum before removing the max val", sum);
-
-sum -= max;
-
-console.log("sum after removing the max val", sum);
-
-let avrg = sum / arr.length;
-
-console.log("And the average is:", avrg);
+console.log("And the average is:", AverageExMax(arr));
