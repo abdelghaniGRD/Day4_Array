e7408752282665eca0477e5bd170e976fca7f824
abdel-ghani <gouroudabdelghani@gmail.com>
2026-09-22 17:00:07 +0100

    solve the exercice 35

---
+++ 35.js
@@ -0,0 +1,12 @@
+let arr = [1, 2, 3, 4, 5, 6];
+
+let even = [];
+let odd = [];
+
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] % 2 === 0) even.push(arr[i]);
+  else odd.push(arr[i]);
+}
+
+console.log("evens", even);
+console.log("odds", odd);
